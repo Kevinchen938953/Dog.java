2ac6781838e30d0cb83b75c743ed102ffc15faeb
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2022-02-11 09:29:03 -0500

    Update DogTester.java

--- DogTester.java
+++ DogTester.java
@@ -1,1 +1,26 @@
+import java.util.ArrayList;
+
+public class DogTester
+{
+	
+	public static void main(String[] args)
+	{
+		ArrayList<Dog> dogArr = new ArrayList<>();
+		Dog d1 = new Dog("Billy", 2005);
+		Dog d2 = new Dog("Charlie", 2001);
+		Poodle p1 = new Poodle("Sally", 2004);
+		Poodle p2 = new Poodle("Dylan", 2008);
+		Bassett b1 = new Bassett("Phillip", 2009);
+		Bassett b2 = new Bassett("Lauren", 2006);
+		p1.eats("Bacon");
+		dogArr.add(p1);
+		dogArr.add(p2);
+		dogArr.add(d1);
+		dogArr.add(d2);
+		dogArr.add(b1);
+		dogArr.add(b2);
+		System.out.print(dogArr);
+		
+	}
+}
 
